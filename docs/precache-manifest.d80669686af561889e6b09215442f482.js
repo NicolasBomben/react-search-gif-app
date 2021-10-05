999cdb621dc3ab9ab0294d37cb44c6e68a7f76a1
BombenDev <nicolasbomben@gmail.com>
2021-10-05 18:00:29 -0300

    added docs

---
+++ docs/precache-manifest.d80669686af561889e6b09215442f482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d43ffb300bc0593de66d503ae9658c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16cc72cb04fb1680ff5a",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16cc72cb04fb1680ff5a",
+    "url": "/static/js/main.89592e45.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
